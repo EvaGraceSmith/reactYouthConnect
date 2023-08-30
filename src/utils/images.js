a90542ea52e6cd64c51c5fa9be777274b4ffb212
EvaGraceSmith <smithevagrace@gmail.com>
2023-08-30 15:10:06 -0500

    added images.js

---
+++ src/utils/images.js
@@ -0,0 +1,8 @@
+export const bgImageLight = {
+    uri: 'https://github.com/YouthConnect/youth-connect-frontend/blob/camera3/public/splashNLVL.png',
+  };
+  
+  export const bgImageDark = {
+    uri: 'https://github.com/YouthConnect/youth-connect-frontend/blob/camera3/public/splashNLVD.png',
+  };
+  
