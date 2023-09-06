e342eb7acb71c46ab0d37187fcf855b20f0cf712
EvaGraceSmith <smithevagrace@gmail.com>
2023-09-06 10:44:29 -0500

    removed all native base and converted to Mui in login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,114 +1,98 @@
-import React, { useContext, useState } from 'react';
-
+import { useContext, useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import { TextField } from '@mui/material';
+import { TextField, Typography } from '@mui/material';
+import base64 from 'base-64';
+import ThemedBackground from '../components/ThemedBackground';
+import { ThemeContext, UserContext } from '../App';
 
+export default function Login({ navigation }) {
+  const { themeInputStyle, themeTextStyle, themeButtonStyle } =
+    useContext(ThemeContext);
+  const { user, setUser } = useContext(UserContext);
 
-import {
-    Box,
-    FormControl,
-    Input,
-    Center,
-    Heading,
-    VStack,
-    Button,
-  } from 'native-base';
-  
-  import { ThemeContext, UserContext } from '../App';
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  
-  import base64 from 'base-64';
-  import ThemedBackground from '../components/ThemedBackground';
-  
-  export default function Login({ navigation }) {
-    const { themeInputStyle, themeTextStyle, themeButtonStyle } =
-      useContext(ThemeContext);
-    const { user, setUser } = useContext(UserContext);
-  
-    const handleSubmit = e => {
-      e.preventDefault();
-  
-      try {
-        let headers = new Headers();
-        let user = base64.encode(`${username}:${password}`);
-        headers.set('Authorization', `Basic ${user}`);
-        fetch('https://youth-connect-backend.onrender.com/signin', {
-          method: 'POST',
-          headers: headers,
-        })
-          .then(res => res.json())
-          .then(data => {
-            console.log('login success');
-            setUser(data.user);
-            navigation.navigate('Home');
-          });
-      } catch (error) {
-        console.log('ERROR SIGNING IN: ', error);
-      }
-    };
-  
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-  
-    return (
-      <ThemedBackground>
-        <Box 
+    try {
+      let headers = new Headers();
+      let user = base64.encode(`${username}:${password}`);
+      headers.set('Authorization', `Basic ${user}`);
+      fetch('https://youth-connect-backend.onrender.com/signin', {
+        method: 'POST',
+        headers: headers,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log('login success');
+          setUser(data.user);
+          navigation.navigate('Home');
+        });
+    } catch (error) {
+      console.log('ERROR SIGNING IN: ', error);
+    }
+  };
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <ThemedBackground>
+      <Box
         display="flex"
         flexDirection="column"
         alignItems="center"
         justifyContent="center"
-        width='100%' 
+        width="100%"
+      >
+        <Box
+          p={2}
+          pt={20}
+          width="90%"
+          maxWidth={290}
         >
-          <Box
-            safeArea
-            p='2'
-            py='20'
-            w='90%'
-            maxW='290'
+          <Typography
+            variant="h6"
+            fontWeight="medium"
+            component="div"
+            mt={1}
+            sx={themeTextStyle}
           >
-            <Heading
-              mt='1'
-              style={themeTextStyle}
-              fontWeight='medium'
-              size='xs'
-            >
-              {user?.username
-                ? `You're all set ${user?.username}`
-                : 'Sign in to continue!'}
-            </Heading>
-  
-            <Box display="flex" flexDirection="column"
-            >
-              <form>
-                <TextField>Username</TextField>
-                <Input
-                  style={themeInputStyle}
-                  onChangeText={setUsername}
-                />
-              </form>
-              <form>
-                <TextField
-                style={themeInputStyle}
-                type='password'
-                onChangeText={setPassword}
-                >Password</TextField>
-  
+            {user?.username
+              ? `You're all set ${user?.username}`
+              : 'Sign in to continue!'}
+          </Typography>
 
-              </form>
-              <Button
-                mt='2'
-                style={[themeButtonStyle]}
-                onPress={handleSubmit}
-                disabled={user?.username ? true : false}
-              >
-                Sign in
-              </Button>
-            </Box>
-          </Box>
+          <form>
+            <TextField
+              label="Username"
+              fullWidth
+              style={themeInputStyle}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </form>
+          <form>
+            <TextField
+              label="Password"
+              type="password"
+              fullWidth
+              style={themeInputStyle}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </form>
+          <Button
+            mt={2}
+            variant="contained"
+            style={themeButtonStyle}
+            onClick={handleSubmit}
+            disabled={user?.username ? true : false}
+          >
+            Sign in
+          </Button>
         </Box>
-      </ThemedBackground>
-    );
-  }
-  
+      </Box>
+    </ThemedBackground>
+  );
+}
