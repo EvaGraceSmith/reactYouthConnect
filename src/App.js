607c6726a6ee4c713acad0b2c5c2a06d1d9f1772
EvaGraceSmith <smithevagrace@gmail.com>
2023-09-06 14:36:22 -0500

    updated App to contain MUI elements and removed native base

--- src/App.js
+++ src/App.js
@@ -1,110 +1,106 @@
-import React, { useState, useEffect, createContext } from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { NativeBaseProvider } from 'native-base';
-import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
+import React, { createContext, useEffect, useState } from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import TabNav from './components/TabNav';
-
 import { useColorScheme } from 'react-native';
-import { bgImageDark, bgImageLight } from './utils/images';
-import { styles } from './utils/styles';
+import { fetchRooms } from './utils/APIFunctions';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider as MuiThemeProvider } from '@mui/styles';
 
 export const ThemeContext = createContext();
 export const UserContext = createContext();
 
-export function fetchRooms(setRooms) {
-  try {
-    fetch('https://youth-connect-backend.onrender.com/api/v1/rooms')
-      .then(res => res.json())
-      .then(data => {
-        setRooms(data);
-      });
-  } catch (err) {
-    console.error('ERROR FETCHING ROOMS: ', err);
-  }
-}
+const theme = createTheme({
+  palette: {
+    type: 'light', // Set 'dark' for dark mode
+  },
+  // Add your theme-specific styles here
+  customStyles: {
+    darkContainer: {
+      backgroundColor: '#333',
+    },
+    darkThemeText: {
+      color: '#fff',
+    },
+    darkThemeButton: {
+      backgroundColor: '#555',
+      color: '#fff',
+    },
+    darkNav: {
+      backgroundColor: '#222',
+    },
+    darkInput: {
+      color: '#fff',
+    },
+    // Define your dark theme styles
+    lightContainer: {
+      backgroundColor: '#fff',
+    },
+    lightThemeText: {
+      color: '#000',
+    },
+    lightThemeButton: {
+      backgroundColor: '#007bff',
+      color: '#fff',
+    },
+    lightNav: {
+      backgroundColor: '#007bff',
+      color: '#fff',
+    },
+    lightInput: {
+      color: '#000',
+    },
+    // Define your light theme styles
+  },
+});
 
-function App() {
+export default function App() {
   const [user, setUser] = useState(null);
   const [room, setRoom] = useState('none');
   const [rooms, setRooms] = useState([]);
-  const [bgImage, setBgImage] = useState();
-  const [colorScheme, setColorScheme] = useState('light'); // Default to light theme
-  const [themeContainerStyle, setThemeContainerStyle] = useState(styles.lightContainer);
-  const [themeTextStyle, setThemeTextStyle] = useState(styles.lightThemeText);
-  const [themeButtonStyle, setThemeButtonStyle] = useState(styles.lightThemeButton);
-  const [themeNavStyle, setThemeNavStyle] = useState(styles.lightNav);
-  const [themeInputStyle, setThemeInputStyle] = useState(styles.lightInput);
+  const [colorScheme, setColorScheme] = useState('light'); // Set the initial color scheme to 'light'
   const [pickedImagePath, setPickedImagePath] = useState();
 
   useEffect(() => {
     fetchRooms(setRooms);
   }, []);
 
+  function setToDarkTheme() {
+    setColorScheme('dark');
+  }
+
+  function setToLightTheme() {
+    setColorScheme('light');
+  }
+
   const toggleTheme = () => {
-    if (colorScheme === 'dark') {
-      setThemeContainerStyle(styles.lightContainer);
-      setThemeTextStyle(styles.lightThemeText);
-      setBgImage(bgImageLight);
-      setColorScheme('light');
-      setThemeButtonStyle(styles.lightThemeButton);
-      setThemeNavStyle(styles.lightNav);
-      setThemeInputStyle(styles.lightInput);
-    } else {
-      setThemeContainerStyle(styles.darkContainer);
-      setThemeTextStyle(styles.darkThemeText);
-      setBgImage(bgImageDark);
-      setColorScheme('dark');
-      setThemeButtonStyle(styles.darkThemeButton);
-      setThemeNavStyle(styles.darkNav);
-      setThemeInputStyle(styles.darkInput);
-    }
+    colorScheme === 'dark' ? setToLightTheme() : setToDarkTheme();
   };
 
-  const inset = {
-    frame: { x: 0, y: 0, width: 0, height: 0 },
-    insets: { top: 0, left: 0, right: 0, bottom: 0 },
-  };
+  useEffect(() => {
+    colorScheme === 'dark' ? setToDarkTheme() : setToLightTheme();
+  }, [colorScheme]);
 
   return (
-    <SafeAreaProvider
-      style={{ paddingTop: 40, ...themeNavStyle }}
-      initialMetrics={inset}
-    >
-      <StatusBar style='dark' hidden={false} />
-      <NavigationContainer>
-        <NativeBaseProvider initialWindowMetrics={inset}>
-          <UserContext.Provider
-            value={{
-              user,
-              setUser,
-              room,
-              setRoom,
-              rooms,
-              setRooms,
-              pickedImagePath,
-              setPickedImagePath,
-            }}
-          >
-            <ThemeContext.Provider
-              value={{
-                colorScheme,
-                bgImage,
-                toggleTheme,
-                themeContainerStyle,
-                themeTextStyle,
-                themeInputStyle,
-                themeNavStyle,
-                themeButtonStyle,
-              }}
-            >
-              <TabNav user={user} room={room} themeNavStyle={themeNavStyle} />
-            </ThemeContext.Provider>
-          </UserContext.Provider>
-        </NativeBaseProvider>
-      </NavigationContainer>
-    </SafeAreaProvider>
+
+    <MuiThemeProvider theme={theme}>
+          <CssBaseline />
+      <UserContext.Provider
+        value={{
+          user,
+          setUser,
+          room,
+          setRoom,
+          rooms,
+          setRooms,
+          pickedImagePath,
+          setPickedImagePath,
+        }}
+      >
+        <ThemeProvider theme={theme}>
+          <TabNav user={user} room={room} />
+        </ThemeProvider>
+      </UserContext.Provider>
+    </MuiThemeProvider>
   );
 }
-
-export default App;
